d47ae4f88385eb0f18c4fa6004d4043dd1f4ba5f
Nikita Voloshyn <nikita.voloshyn.odessa@gmail.com>
2023-04-01 12:16:33 +0300

    Конечная версия TimeWarp

--- TimeWarp/TimeWarp/ContentView.swift
+++ TimeWarp/TimeWarp/ContentView.swift
@@ -3,56 +3,103 @@
 //  TimeWarp
 //
 //  Created by Волошин Нікіта on 30.03.2023.
-
 //
 
-//
+
 
 import SwiftUI
+import os
 
+let logger = Logger()
+// ЛОГЕР
 
+//.onChange(of: selectedTypeOfTimeFrom) { tag in
+//    logger.log("selectedTypeOfTimeFrom \(tag)")
+//}
 
-struct ContentView: View {
+enum TimeUnit: String, CaseIterable {
+    case seconds
+    case minutes
+    case hours
+    case days
     
-    @State private var typesOfTime = ["second", "minutes", "hours", "days"]
-    @State private var inputAmount = ""
-    
-    
-    var body: some View {
-            Form{
-                Section{ //выбор входного значения + текстовое поле для ввода
-                    Picker("", selection: $typesOfTime){
-                        ForEach(typesOfTime, id: \.self){
-                            Text($0)
-                        }
-                    }
-                    .pickerStyle(.wheel)
-                    .frame(width: 350, height: 140)
-                    TextField("Enter quantity", text: $inputAmount)
-                }
-            header: {Text("Сhoose which type you will convert")}  // название
-                    .navigationTitle("TimeWrap")
-                    .padding(.top)
-                
-                
-                Section{ // выбор выходного значения
-                    Picker("", selection: $typesOfTime){
-                        ForEach(typesOfTime, id: \.self){
-                            Text($0)
-                        }
-                    }
-                    .pickerStyle(.wheel)
-                    .frame(width: 350, height: 170)
-                }
-            header: {Text("Choose the type you want to get")}
-                
-                Section{
-                    Text("")
-                } header: {Text("Your result in:")}
-            }
+    var seconds: Double {
+        switch self {
+        case .seconds:
+            return 1
+        case .minutes:
+            return 60
+        case .hours:
+            return 3600
+        case .days:
+            return 86400
         }
     }
+}
 
+struct ContentView: View {
+    @FocusState private var amoundIsFocused: Bool // настройка клавы
+    
+    @State private var selectedTypeOfTimeFrom: TimeUnit = .seconds
+    @State private var selectedTypeOfTimeTo: TimeUnit = .seconds
+    @State private var inputValue = ""
+    
+    var convertedValue: Double { // логика конвертации
+        let inputValue = Double(self.inputValue) ?? 0
+        let inputSeconds = inputValue * selectedTypeOfTimeFrom.seconds
+        let outputSeconds = inputSeconds / selectedTypeOfTimeTo.seconds
+        return outputSeconds
+    }
+    
+    var body: some View {
+        NavigationView{
+            Form{
+                Section{
+                    Picker("Input unit", selection: $selectedTypeOfTimeFrom){
+                        ForEach(TimeUnit.allCases, id: \.self){
+                            Text($0.rawValue)
+                        }
+                    }
+                    .pickerStyle( .wheel)
+                    .frame(width: 350, height: 120)
+                    
+                    TextField("Enter quantity", text: $inputValue)
+                        .keyboardType(.decimalPad)
+                        .focused($amoundIsFocused)
+                }
+                header: {Text("Сhoose which type you will convert")}
+                
+                Section{
+                    Picker("Output unit", selection: $selectedTypeOfTimeTo){
+                        ForEach(TimeUnit.allCases, id: \.self){
+                            Text($0.rawValue)
+                        }
+                    }
+                    .frame(width: 350, height: 160)
+                    .pickerStyle( .wheel)
+                }
+                header: {Text("Choose the type you want to get")}
+                
+                .toolbar { // способность закрыть клаву
+                    ToolbarItemGroup(placement: .keyboard) {
+                        Spacer()
+                        
+                        Button("Done") {
+                            amoundIsFocused = false
+                        }
+                    }
+                }
+                
+                Section{
+                    Text("\(convertedValue)")
+                }
+                header: {Text("Result")}
+            }
+            .navigationTitle("TimeWrap")
+            .padding(.top)
+        }
+    }
+}
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
